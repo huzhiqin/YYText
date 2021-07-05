e031755e1e958f70e797d250eed86c9f89db4031
huzhiqin <136156265@qq.com>
2021-07-05 11:44:39 +0800

    Update YYTextAttribute.m
            CTFontRef ctFont = CTFontCreateWithName([font.fontName isEqualToString:@".SFUI-Regular"] ? (__bridge CFStringRef)@"TimesNewRomanPSMT":(__bridge CFStringRef)font.fontName, font.pointSize, NULL);

--- YYText/String/YYTextAttribute.m
+++ YYText/String/YYTextAttribute.m
@@ -447,7 +447,7 @@ YYTextAttributeType YYTextAttributeGetType(NSString *name){
     if (font == (id)[NSNull null] || font == nil) {
         ((NSMutableDictionary *)_attributes)[(id)kCTFontAttributeName] = [NSNull null];
     } else {
-        CTFontRef ctFont = CTFontCreateWithName((__bridge CFStringRef)font.fontName, font.pointSize, NULL);
+        CTFontRef ctFont = CTFontCreateWithName([font.fontName isEqualToString:@".SFUI-Regular"] ? (__bridge CFStringRef)@"TimesNewRomanPSMT":(__bridge CFStringRef)font.fontName, font.pointSize, NULL);
         if (ctFont) {
             ((NSMutableDictionary *)_attributes)[(id)kCTFontAttributeName] = (__bridge id)(ctFont);
             CFRelease(ctFont);
